7bccf5b7c0ec947808ae3e15f1db0c398255b8e2
Don Park <donp@donp.org>
2024-11-23 12:11:26 +0700

    use the hash, Luke

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -6,15 +6,19 @@
 
   onMount(async () => {
     let request = JSON.stringify({ start_timestamp: 0 });
-    images = await fetch(PUBLIC_IMG_API_URL, {
+    let images_resp = await fetch(PUBLIC_IMG_API_URL, {
       method: "post",
       body: request,
     }).then((ps) => ps.json());
+    images.push(...images_resp.images);
   });
 </script>
 
 <h3>img-gallery</h3>
+
+{images.length} images
+
 {#each images as image}
   image {JSON.stringify(image)}
-  <img src="{PUBLIC_IMG_STORE_URL}/{image.filename}" alt={image.filename} />
+  <img src="{PUBLIC_IMG_STORE_URL}/{image.hash}" alt={image.filename} />
 {/each}
