03f23348ea9122a0e86a14d411e9fa08e61598f9
Don Park <donp@donp.org>
2024-11-24 11:14:42 +0700

    html stuff

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -3,22 +3,45 @@
   import { PUBLIC_IMG_API_URL, PUBLIC_IMG_STORE_URL } from "$env/static/public";
 
   let images = $state([]);
+  let loading = $state(false);
 
   onMount(async () => {
+    loading = true;
     let request = JSON.stringify({ start_timestamp: 0 });
     let images_resp = await fetch(PUBLIC_IMG_API_URL, {
       method: "post",
       body: request,
     }).then((ps) => ps.json());
     images.push(...images_resp.images);
+    loading = false;
   });
 </script>
 
 <h3>img-gallery</h3>
 
-{images.length} images
+<div>
+  {#if loading}
+    Loading
+  {:else}
+    {images.length} images
+  {/if}
+</div>
 
-{#each images as image}
-  image {JSON.stringify(image)}
-  <img src="{PUBLIC_IMG_STORE_URL}/{image.hash}" alt={image.filename} />
-{/each}
+<div id="images">
+  {#each images as image}
+    <div class="image">
+      <img src="{PUBLIC_IMG_STORE_URL}/{image.hash}" alt={image.filename} />
+      {image.hash}
+    </div>
+  {/each}
+</div>
+
+<style>
+  .image {
+  }
+
+  #images {
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
