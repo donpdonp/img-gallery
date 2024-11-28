dff4deb446c0d90ef166886ef223ab9fb9664876
Don Park <donp@donp.org>
2024-11-28 10:21:05 +0700

    multithreaded take 1

--- fileserve/src/main.rs
+++ fileserve/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread::spawn;
+
 use tiny_http::{Header, Server};
 
 use fileserve::{db, router::route_request};
@@ -11,15 +13,17 @@ fn main() -> Result<(), std::io::Error> {
 
     println!("listening {}", &config.listen_address);
     let server = Server::http(&config.listen_address).unwrap();
-    let mut db = db::init();
 
     // accept connections and process them serially
     for mut request in server.incoming_requests() {
         println!("method: {:?}, url: {:?}", request.method(), request.url(),);
 
-        let resp = route_request(&mut db, &mut request);
-        let cors = Header::from_bytes("Access-Control-Allow-Origin", "*").unwrap();
-        request.respond(resp.with_header(cors)).unwrap();
+        spawn(|| {
+            let mut db = db::init();
+            let resp = route_request(&mut db, &mut request);
+            let cors = Header::from_bytes("Access-Control-Allow-Origin", "*").unwrap();
+            request.respond(resp.with_header(cors)).unwrap();
+        });
     }
     Ok(())
 }
