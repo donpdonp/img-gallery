fc8dd41662b78c51e7bc04d69d03e40ef18694bf
Don Park <donp@donp.org>
2024-11-30 13:21:51 +0700

    cleanup

--- fileserve/src/router.rs
+++ fileserve/src/router.rs
@@ -18,40 +18,44 @@ pub struct ImageListResp {
     images: Vec<Image>,
 }
 
-pub fn route_request(mut db: &mut Connection, request: &mut Request) -> Response<Cursor<Vec<u8>>> {
+pub fn route_request(db: &mut Connection, request: &mut Request) -> Response<Cursor<Vec<u8>>> {
     let json_opt = parse_request(request);
-    let mut body = String::new();
     if let Some(json) = json_opt {
         println!("body: {}", json);
         let req: Req = serde_json::from_str(&json).unwrap();
-        let images = db::images_since(db, req.start_timestamp, req.stop_timestamp);
-        let req_resp = ImageListResp { images };
-        body.push_str(&serde_json::to_string(&req_resp).unwrap());
-        let content_type = Header::from_bytes("Content-Type", "application/json").unwrap();
-        Response::from_string(body).with_header(content_type)
+        image_gallery(db, req)
     } else {
-        let url = Url::parse(&("http://localhost".to_owned() + request.url())).unwrap();
-        let hash_code = url.path();
-        let hash = shared::hash::hash_to_u64(&hash_code[1..]);
-        let config = shared::CONFIG.get().unwrap();
-        let img_bytes = match db::image_exists(db, hash) {
-            Some(image) => {
-                let new_height = url
-                    .query_pairs()
-                    .find(|qp| qp.0 == "h")
-                    .map(|qp| u32::from_str_radix(&qp.1, 10).unwrap());
-                let filename = config.photos_path.clone() + "/" + &image.filename;
-                println!("thumbnail processing {:?}", filename);
-                thumbnail(&mut db, filename, new_height)
-            }
-            None => vec![],
-        };
-        let content_type = Header::from_bytes("Content-Type", "image/jpeg").unwrap();
-        Response::from_data(img_bytes).with_header(content_type)
+        thumbnail(db, request)
     }
 }
 
-fn thumbnail(_db: &mut Connection, filename: String, height: Option<u32>) -> Vec<u8> {
-    let file_bytes = std::fs::read(filename).unwrap();
-    image_thumb(&file_bytes, height).unwrap()
+fn image_gallery(db: &mut Connection, req: Req) -> Response<Cursor<Vec<u8>>> {
+    let mut body = String::new();
+    let images = db::images_since(db, req.start_timestamp, req.stop_timestamp);
+    let req_resp = ImageListResp { images };
+    body.push_str(&serde_json::to_string(&req_resp).unwrap());
+    let content_type = Header::from_bytes("Content-Type", "application/json").unwrap();
+    Response::from_string(body).with_header(content_type)
+}
+
+fn thumbnail(db: &mut Connection, request: &mut Request) -> Response<Cursor<Vec<u8>>> {
+    let url = Url::parse(&("http://localhost".to_owned() + request.url())).expect("bad url");
+    let hash_code = url.path();
+    let hash = shared::hash::hash_to_u64(&hash_code[1..]);
+    let config = shared::CONFIG.get().unwrap();
+    let img_bytes = match db::image_exists(db, hash) {
+        Some(image) => {
+            let new_height = url
+                .query_pairs()
+                .find(|qp| qp.0 == "h")
+                .map(|qp| u32::from_str_radix(&qp.1, 10).unwrap());
+            let filename = config.photos_path.clone() + "/" + &image.filename;
+            println!("thumbnail processing {:?}", filename);
+            let file_bytes = std::fs::read(filename).unwrap();
+            image_thumb(&file_bytes, new_height).unwrap()
+        }
+        None => vec![],
+    };
+    let content_type = Header::from_bytes("Content-Type", "image/jpeg").unwrap();
+    Response::from_data(img_bytes).with_header(content_type)
 }
